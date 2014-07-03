7b91a16af394543523d105ee0e6a1c6db0be15b6
coolboyssahil <coolboys.sahil@gmail.com>
2014-07-03 15:17:29 +0530

    Initialized branch

---
+++ js/background.js
@@ -0,0 +1,19 @@
+chrome.webRequest.onBeforeSendHeaders.addListener(
+	function(details) {
+		var ua = JSON.parse(localStorage["currentUA"]).ua;
+		var enabled = localStorage["enabled"];
+		if(enabled && ua.toLowerCase() != "default" && ua.length != 0 && ua != undefined)
+		{
+			for (var i = 0; i < details.requestHeaders.length; ++i) {
+				if (details.requestHeaders[i].name === 'User-Agent') {
+					details.requestHeaders[i].value = ua;
+					break;
+				}
+			}
+		}
+		return {
+			requestHeaders: details.requestHeaders
+		};
+	}, {
+		urls: ["<all_urls>"]
+	}, ["blocking", "requestHeaders"]);
